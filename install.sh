a7907fb95f34ab4f7250636321f0be6a85107388
Quentin Forcioli <quentin.forcioli@telecom-paris.fr>
2023-04-28 18:50:23 +0200

    rename module

--- install.sh
+++ install.sh
@@ -13,6 +13,6 @@ gnome-extensions pack ./ \
     --force \
 
 echo "Installing extension..."
-gnome-extensions install Battery-Health-Charging@maniacx.github.com.shell-extension.zip --force
+gnome-extensions install Battery-Health-Charging@qlala.github.com.shell-extension.zip --force
 
 echo "Extension installed Battery Health Charging succesfully. Restart the shell (or logout) to be able to enable the extension."
